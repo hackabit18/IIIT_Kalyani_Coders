65ad54e1eae154ffdba05a8e19460c008ab303ef
Anubhav Patel <anubhavp28@gmail.com>
2018-10-28 02:57:14 +0530

    i am sleepy

--- hackproduct.py
+++ hackproduct.py
@@ -11,6 +11,10 @@ def manifest():
 	return redirect(url_for('static', filename='manifest.json'))
 
 @app.route('/')
+def index():
+    return redirect(url_for('trending'))
+
+@app.route('/trending')
 def trending():
     conn = sqlite3.connect('db.sqlite')
     c = conn.cursor()
@@ -75,7 +79,43 @@ def do_analysis():
 	        "content": data['content']
         })
         j = json.loads(r.text)
-        return render_template('analyse_result.html', url = request.form['url'], title = data['title'],  dtitle = j['title']["decision"], dcontent = j['content']["decision"])
+    
+        conn = sqlite3.connect('db.sqlite')
+        c = conn.cursor()
+        c.execute("SELECT * FROM politicians")
+        politicians = c.fetchall()
+
+        contains = []
+
+        for politician in politicians:
+            if data['content'].count(politician[1]) > 0 or data['title'].count(politician[1]) > 0:
+                contains.append(politician[1])
+
+        return render_template('analyse_result.html', url = request.form['url'], title = data['title'],  dtitle = j['title']["decision"], dcontent = j['content']["decision"], contains = contains)
     return 'ERROR : URL Not Supported!'
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/do-search', methods=['POST'])
+def do_search():
+    conn = sqlite3.connect('db.sqlite')
+    c = conn.cursor()
+    #c.execute('SELECT * from `articles` WHERE title LIKE %%%s%%' % request.form['term'])
+    #data = c.fetchall()
+    c.execute('SELECT `articles`.*, `fakebox`.`dtitle`, `fakebox`.`dcontent`, `mediahouse`.`mhname` \
+    from `articles`, `fakebox`, `mediahouse` WHERE LOWER(`articles`.`title`) LIKE \'%%%s%%\' AND `articles`.`article_id` = `fakebox`.`article_id` AND `articles`.`mhid` = `mediahouse`.`mhid`;' % request.form['term'].lower() )
+
+    #c.execute('SELECT `articles`.`article_id`, `articles`.`url`, `articles`.`title`, `articles`.`mhid`, `fakebox`.`dtitle`, `fakebox`.`dcontent`, `mediahouse`.`mhname` \
+    #from `articles`, `fakebox`, `mediahouse` WHERE LOWER(`articles`.`title`) LIKE \'%%%s%%\' AND `articles`.`article_id` = `fakebox`.`article_id` AND `articles`.`mhid` = `mediahouse`.`mhid`;' % request.form['term'] )
+    articles = c.fetchall()
+    contains_list = []
+    for article in articles:
+        c.execute('SELECT `sentiments`.`pid`, `politicians`.`pname` FROM `sentiments`, `politicians` WHERE `sentiments`.`article_id`=%d AND `sentiments`.`pid` = `politicians`.`pid`;' %
+        article[0])
+        contains_list.append(c.fetchall())
+
+    return render_template('search_result.html', var = articles , contains = contains_list)
 
 app.run(host="0.0.0.0", debug=True)
